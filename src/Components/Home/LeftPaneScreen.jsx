ab94c9566be88678c1fa121ea0e1b8df46fb132c
Siddhu1925 <114770885+Siddhu1925@users.noreply.github.com>
2023-01-20 13:43:10 +0530

    Update LeftPaneScreen.jsx

--- src/Components/Home/LeftPaneScreen.jsx
+++ src/Components/Home/LeftPaneScreen.jsx
@@ -5,7 +5,7 @@ function LeftPaneScreen() {
   return (
     <div className=' h-screen flex justify-end bg-black'>
       <div className='mx-auto flex flex-col items-center justify-center gap-3 text-center'>
-        <img src ='./logo12-removebg-preview.png' alt='logo' className='w-16 h-20' />
+        <img src ='./logo12.png' alt='logo' className='w-16 h-20' />
         <h3 className='font-semibold text-white'> Code Deck</h3>
         <h4 className='font-semibold text-white'> Code. Compile. Debug</h4>
         <button className='w-full p-4 bg-white shadow-lg rounded-full drop-shadow-2xl'>
